26dddb19b3027bb84cca948d991c79ebaae9c20b
Guillermo.N <106453304+Guillermo-N@users.noreply.github.com>
2022-05-29 09:41:26 +0200

    Adding home and about templates.

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Guillermo N",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "May 29, 2022",
+    },
+    {
+        "author": "Tulio Fra",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "May 30, 2022",
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html", title="About")
 
 
 
