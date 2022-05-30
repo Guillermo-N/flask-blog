41617a23a37fbd70fee3d8b144dafd54a9304f66
Guillermo.N <106453304+Guillermo-N@users.noreply.github.com>
2022-05-30 17:16:45 +0200

    Adding forms (Registration and LogIn) and security key.

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "29b88f280b5ab6fbc8996226930c6e0b" # secrets.token_hex(16)
 
 posts = [
     {
