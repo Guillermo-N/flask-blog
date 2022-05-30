2857d890e164ad557b822bff5ce93a14f79d5cc0
Guillermo.N <106453304+Guillermo-N@users.noreply.github.com>
2022-05-30 19:05:22 +0200

    Login page and prompts.

--- flaskblog.py
+++ flaskblog.py
@@ -42,14 +42,22 @@ def register():
     if form.validate_on_submit():
         flash(f"Account created for {form.username.data}!", "success")
         return redirect(url_for("home"))
-        
+
     return render_template("register.html", title="Register", form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
 
     form = LoginForm()
+
+    if form.validate_on_submit():
+        if form.email.data == "test@example.com" and form.password.data == "password":
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check username and password", "danger")
+            
     return render_template("login.html", title="LogIn", form=form)
 
 
