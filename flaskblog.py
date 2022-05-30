a987c98e333a8166572d246187db5e59f841040f
Guillermo.N <106453304+Guillermo-N@users.noreply.github.com>
2022-05-30 18:17:53 +0200

    Accepting GET and POST on registration page, flashing when new registration.

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from crypt import methods
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -23,21 +24,34 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
+
     return render_template("home.html", posts=posts)
+
 
 @app.route("/about")
 def about():
+
     return render_template("about.html", title="About")
 
-@app.route("/register")
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
+
     form = RegistrationForm()
+
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+        
     return render_template("register.html", title="Register", form=form)
+
 
 @app.route("/login")
 def login():
+
     form = LoginForm()
     return render_template("login.html", title="LogIn", form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
