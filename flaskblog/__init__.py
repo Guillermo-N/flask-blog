7f100a6c8eecad9e768f9f1924b111b84ec89011
Guillermo.N <106453304+Guillermo-N@users.noreply.github.com>
2022-06-01 12:25:30 +0200

    Refactoring imports

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "29b88f280b5ab6fbc8996226930c6e0b" # secrets.token_hex(16)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
+
+from flaskblog import routes
