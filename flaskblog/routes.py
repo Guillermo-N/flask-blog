0ad8315dedea412dfb84ea697a63f5367db30b5f
Guillermo.N <106453304+Guillermo-N@users.noreply.github.com>
2022-06-02 12:37:19 +0200

    Login check for db entry.

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 from flaskblog import app, db, bcrypt
+from flask_login import login_user
 
 posts = [
     {
@@ -53,10 +54,11 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        if form.email.data == "test@example.com" and form.password.data == "password":
-            flash("You have been logged in!", "success")
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for("home"))
         else:
-            flash("Login Unsuccessful. Please check username and password", "danger")
+            flash("Login Unsuccessful. Please check email and password", "danger")
             
     return render_template("login.html", title="LogIn", form=form)
